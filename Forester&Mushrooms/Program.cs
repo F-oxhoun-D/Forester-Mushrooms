435927c4b406a5e0a516f8e54d823aed268eb6e5
Nikita <shchukin_nikita02@mail.ru>
2024-06-08 13:32:04 +0300

    Добавьте файлы проекта.

---
+++ Forester&Mushrooms/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Forester_Mushrooms
+{
+    internal class Program
+    {
+        static char[,]? field;
+
+        static readonly string pathToTheFileDescribingTheTask = "info.txt";
+
+        static int n = 1;
+
+        static void Main()
+        {
+            using StreamReader reader = new(pathToTheFileDescribingTheTask);
+            string? lineOfTheFile;
+            while ((lineOfTheFile = reader.ReadLine()) != null)
+                Console.WriteLine(lineOfTheFile);
+
+            Console.Write("\n\n\nРазмерноость поля n = ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+                Console.Write("Введите число ");
+            field = new char[n, 3];
+
+            Console.WriteLine("\nЗаполните клетки поля (возможны только символы '.', 'W' и 'C'");
+            FillInTheField();
+            
+        }
+
+        static void FillInTheField()
+        {
+            int numberLine = 1;
+            while (numberLine != n + 1)
+            {
+                bool checkCorrectLine = false;
+                while (!checkCorrectLine)
+                {
+                    Console.WriteLine($"{numberLine}-я строка: ");
+                    string line = Console.ReadLine()!;
+                    if (line != string.Empty)
+                    {
+                        if (line.Length == 3)
+                        {
+                            for (int numberColumn = 0; numberColumn < line.Length; numberColumn++)
+                            {
+                                char c = line[numberColumn];
+                                if (c is not '.' and not 'W' and not 'C')
+                                    c = GetCorrectCellValue(ref c);
+
+                                field![numberLine - 1, numberColumn] = c;
+                            }
+                            numberLine++;
+                            checkCorrectLine = true;
+                        }
+                        else
+                            Console.WriteLine($"К-во символов {line.Length} меньше необходимого: 3)");
+                    }
+                    else
+                        Console.WriteLine("Введена пустая строка");
+                }
+            }
+        }
+
+        static char GetCorrectCellValue(ref char cellValue)
+        {
+            if (cellValue is not '.' and not 'W' and not 'C')
+            {
+                Console.WriteLine($"Замените символ {cellValue} на один из  трёх: '.', 'W', 'C'");
+                bool checkTheCorrectOfTheCharInput = false;
+                while (!checkTheCorrectOfTheCharInput)
+                {
+                    string enterSymbol = Console.ReadLine()!;
+                    if (enterSymbol != string.Empty)
+                    {
+                        char symbol = Convert.ToChar(enterSymbol);
+                        return GetCorrectCellValue(ref symbol);
+                    }
+                    else
+                        Console.WriteLine("Введён пустой символ");
+                }
+            }
+            return cellValue;
+        }
+
+    }
+}
